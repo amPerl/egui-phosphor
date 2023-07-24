20f159b91eaf5c06a606e45e925b2bb8a4925840
Juan Campa <juancampa@gmail.com>
2023-07-24 23:10:22 +0000

    Support for the 'fill' version of the font behind a feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,18 @@ pub mod icons;
 pub use icons::*;
 
 pub fn font_data() -> egui::FontData {
-    let mut font_data = egui::FontData::from_static(include_bytes!("../res/Phosphor.ttf"));
+    #[cfg(all(feature = "foo", feature = "bar"))]
+    compile_error!("Features \"lines\" and \"filled\" are mutually exclusive ");
+
+    let mut font_data;
+    #[cfg(feature = "lines")]
+    {
+       font_data = egui::FontData::from_static(include_bytes!("../res/Phosphor.ttf"));
+    }
+    #[cfg(feature = "filled")]
+    {
+       font_data = egui::FontData::from_static(include_bytes!("../res/Phosphor-Fill.ttf"));
+    }
     font_data.tweak.y_offset_factor = 0.0;
     font_data
 }
