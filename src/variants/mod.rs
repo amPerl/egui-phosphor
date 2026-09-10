5a4656014c05268d8563a9ea4e768055d6470b3e
Marco Radocchia <mar.radocchia@proton.me>
2026-09-10 14:02:19 +0200

    feat: Make `Variant::font_bytes` const compatible (#43)

--- src/variants/mod.rs
+++ src/variants/mod.rs
@@ -62,7 +62,7 @@ pub enum Variant {
 }
 
 impl Variant {
-    pub fn font_bytes(&self) -> &'static [u8] {
+    pub const fn font_bytes(&self) -> &'static [u8] {
         match self {
             #[cfg(feature = "thin")]
             Variant::Thin => bytes::thin::FONT,
